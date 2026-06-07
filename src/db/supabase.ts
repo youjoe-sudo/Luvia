19d3be58244fe0beeb528436a1f6ac1cd958cf91
AbdoooElfata7 <lpl66448844@gmail.com>
2026-06-07 14:48:53 +0300

    supa

--- src/db/supabase.ts
+++ src/db/supabase.ts
@@ -1,8 +1,18 @@
+console.log("URL:", import.meta.env.VITE_SUPABASE_URL);
+console.log("KEY:", import.meta.env.VITE_SUPABASE_ANON_KEY);
 
-            import { createClient } from "@supabase/supabase-js";
+import { createClient } from "@supabase/supabase-js";
 
-            const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-            const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-            export const supabase = createClient(supabaseUrl, supabaseAnonKey);
-            (window as any).supabase = supabase;
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error("Missing Supabase environment variables");
+}
+
+export const supabase = createClient(
+  supabaseUrl,
+  supabaseAnonKey
+);
+
+(window as any).supabase = supabase;
